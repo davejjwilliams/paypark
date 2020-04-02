24bcf3c687ec9a5b1344941b8da82a6eb3e7bbeb
Williams, Dave J (UG - Computer Science) <dw00506@surrey.ac.uk>
2020-04-02 19:42:03 +0200

    Added Google Sign-In with OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = SecureRandom.hex
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'home/home'
   get 'home/contact'
 
+  # App Root
   root 'home#home'
+
+  # Omniauth Redirect
+  get '/auth/:provider/callback' => 'home#omniauth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
