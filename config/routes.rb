7354c38e8a9f5256224bdf64edbc58150e972759
Williams, Dave J (UG - Computer Science) <dw00506@surrey.ac.uk>
2020-04-06 18:14:20 +0200

    Generated Verification Controller, added Routes, Updated Verification Controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # App Root
   root 'home#home'
 
+  # Verification Form
+  get 'verification', to:'verification#new'
+  post 'verification', to:'verification#verify'
+
   # Omniauth Redirect
   get '/auth/:provider/callback' => 'home#omniauth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
