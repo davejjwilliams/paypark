988fd6a181b9862e103a31fcd7f3bfe11cb1575b
Minasian, Levon A(UG - Computer Science) <lm01108@surrey.ac.uk>
2020-04-08 12:06:02 +0100

    Search and drag

--- app/assets/javascripts/homeowners.js
+++ app/assets/javascripts/homeowners.js
@@ -2,20 +2,8 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-function initMap(lat, lng) {
-    var myCoords = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        center: myCoords,
-        zoom: 14
-    };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    var marker = new google.maps.Marker({
-        position: myCoords,
-        map: map
-    });
-}
-
 function initMap2() {
+
     var lat = document.getElementById('homeowner_latitude').value;
     var lng = document.getElementById('homeowner_longitude').value;
 
@@ -51,6 +39,7 @@ function initMap2() {
     // when input values change call refreshMarker
     document.getElementById('homeowner_latitude').onchange = refreshMarker;
     document.getElementById('homeowner_longitude').onchange = refreshMarker;
+
     // when marker is dragged update input values
     marker.addListener('drag', function () {
         latlng = marker.getPosition();
@@ -63,4 +52,102 @@ function initMap2() {
     marker.addListener('dragend', function () {
         map.panTo(marker.getPosition());
     });
+}
+
+function setCords(){
+    latlng = marker.getPosition();
+    newlat = (Math.round(latlng.lat() * 1000000)) / 1000000;
+    newlng = (Math.round(latlng.lng() * 1000000)) / 1000000;
+    document.getElementById('homeowner_latitude').value = newlat;
+    document.getElementById('homeowner_longitude').value = newlng;
+}
+
+function initMap3() {
+
+    // Spawn map
+    var map = new google.maps.Map(document.getElementById('map2'), {
+        center: {lat: 51.241920, lng: -0.585900},
+        zoom: 14,
+        mapTypeId: 'roadmap'
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    //var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+  /*      markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];*/
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+      /*      // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location,
+         /!*     var latitude = place.geometry.location.lat();
+                var longitude = place.geometry.location.lng();*!/
+                draggable: true
+            }));*/
+
+            var marker = new google.maps.Marker({
+                position: place.geometry.location,
+                animation: google.maps.Animation.DROP,
+                map: map,
+                icon: icon,
+                draggable: true
+            }); setCords();
+
+            // when marker is dragged update input values
+            marker.addListener('drag', function () {
+               setCords();
+            });
+            // When drag ends, center (pan) the map on the marker position
+            marker.addListener('dragend', function () {
+                map.panTo(marker.getPosition());
+            });
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
 }
