21ee4d413a158170fe8d235ac3d4f3895d1d5f6a
Minasian, Levon A(UG - Computer Science) <lm01108@surrey.ac.uk>
2020-04-09 16:25:46 +0100

    drag limit 2

--- app/assets/javascripts/homeowners.js
+++ app/assets/javascripts/homeowners.js
@@ -75,7 +75,7 @@ function initMap3() {
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
-   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    //map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
@@ -98,14 +98,7 @@ function initMap3() {
                 console.log("Returned place contains no geometry");
                 return;
             }
-       /*     var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-*/
+
             // Clear last marker
             marker.setMap(null);
 
@@ -114,8 +107,6 @@ function initMap3() {
                 position: place.geometry.location,
                 animation: google.maps.Animation.DROP,
                 map: map,
-                //icon: icon,
-                //label: 'P',
                 title: place.name,
                 draggable: true
             }); setCords(marker);
@@ -148,5 +139,4 @@ function initMap3() {
         });
         map.fitBounds(bounds);
     });
-
 }
