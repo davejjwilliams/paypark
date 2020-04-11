945bb7c77ca5b2790b6155d39e4979be4a982905
Williams, Dave J (UG - Computer Science) <dw00506@surrey.ac.uk>
2020-04-11 19:06:51 +0200

    Added session variables for homeowner and driver IDs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,19 @@ class HomeController < ApplicationController
     @user = User.from_omniauth(auth)
     @user.save
     session[:user_id] = @user.id
+    puts "THE CURRENT USER ID IS: " + session[:user_id].to_s
     sign_in(User.find(@user.id), scope: :user)
+
+    if Homeowner.exists?(user_id: current_user.id)
+      session[:homeowner_id] = Homeowner.find_by_user_id(current_user.id).id
+      puts "THE CURRENT HOMEOWNER ID IS: " + session[:homeowner_id].to_s
+    end
+
+    if Driver.exists?(user_id: current_user.id)
+      session[:driver_id] = Driver.find_by_user_id(current_user.id).id
+      puts "THE CURRENT DRIVER ID IS: " + session[:driver_id].to_s
+    end
+
     if !Homeowner.exists?(user_id: current_user.id) && !Driver.exists?(user_id: current_user.id)
       redirect_to signup_path
     else
