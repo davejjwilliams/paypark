56ba406ac20f82962824edc6fdb2cf33c37ed987
Minasian, Levon A(UG - Computer Science) <lm01108@surrey.ac.uk>
2020-04-27 11:46:11 +0100

    map search bar & only show valid driveways

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,6 +2,6 @@ class MapController < ApplicationController
   before_action :authenticate_user!
 
   def map
-    gon.driveways = Homeowner.all
+    gon.driveways = Homeowner.all.where(driveway_active: true, driveway_verified: true)
   end
 end
