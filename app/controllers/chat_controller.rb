4aaecd5e2fca527176824f6eee8838069494d0f8
Michael Wayne <mw00885@surrey.ac.uk>
2020-04-28 21:46:47 +0100

    Chat screen now only shows drivers and homeowners who have a booking with you/you have a booking with, that is not yet complete.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,9 +3,14 @@ class ChatController < ApplicationController
 
     session[:conversations] ||= []
 
+    if !Homeowner.find_by_user_id(current_user.id).nil?
+      @homeowner_conversations_with = Driver.all.where(:id => Booking.select(:driver_id).all.where(:homeowner_id => Homeowner.find_by_user_id(current_user.id).id, :complete => false))
+    end
+    @driver_conversations_with = Homeowner.all.where(:id => Booking.select(:homeowner_id).all.where(:driver_id => Driver.find_by_user_id(current_user.id).id, :complete => false))
+
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
-    
+
 
   end
 end
