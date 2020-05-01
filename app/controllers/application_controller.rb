8b4ca414651a0585e0886e2cfba6384330bbb829
Minasian, Levon A(UG - Computer Science) <lm01108@surrey.ac.uk>
2020-05-02 00:53:13 +0100

    websocket fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :setUser
+  def setUser
+    if user_signed_in?
+      gon.user = current_user.name
+    else
+      gon.user = ""
+    end
+  end
 end
