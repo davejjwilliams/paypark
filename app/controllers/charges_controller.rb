41e3efb7315bcd56107c9606a3c34863151bef54
Williams, Dave J (UG - Computer Science) <dw00506@surrey.ac.uk>
2020-05-10 22:31:20 +0200

    Bookings removed from GCal when refunded

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -87,6 +87,29 @@ class ChargesController < ApplicationController
                                        payment_intent: booking.payment_intent,
                                    })
 
+    token = current_user.google_token
+    puts "Current Token Is: #{token.access_token}"
+    # Initialize Google Calendar API
+    service = Google::Apis::CalendarV3::CalendarService.new
+    # Use google keys to authorize
+    service.authorization = token.google_secret.to_authorization
+    # Request for a new access token just in case it expired
+    if token.expired?
+      service.authorization = Signet::OAuth2::Client.new(    { token_credential_uri: 'https://oauth2.googleapis.com/token',
+                                                               access_token: current_user.google_token,
+                                                               expires_at: token.expires_at,
+                                                               refresh_token: token.refresh_token,
+                                                               client_id: Rails.application.credentials.google[:google_client_id],
+                                                               client_secret: Rails.application.credentials.google[:google_client_secret] })
+
+      new_access_token = service.authorization.refresh!
+      token.access_token = new_access_token['access_token']
+      token.expires_at = Time.now.to_i + new_access_token['expires_in'].to_i
+      token.save
+    end
+
+    service.delete_event('primary', booking.calendar_event_id)
+
     # destroy booking
     booking.destroy!
     redirect_to root_path
