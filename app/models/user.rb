4a3c10569875f5f259fa84939e6acc93e0522e31
sc01716@surrey.ac.uk <sc01716@surrey.ac.uk>
2020-05-10 19:43:03 +0100

    signup mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable # :confirmable 
+         :recoverable, :rememberable, :validatable
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
@@ -10,8 +10,15 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = SecureRandom.hex
     end
+
+
   end
 
+  after_create :signup_confirmation
+  def signup_confirmation
+    UserMailer.signup_confirmation(self).deliver
+    
+  end
   # Chat associations
   has_many :conversations, foreign_key: :sender_id
   has_many :messages
--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_010146) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
