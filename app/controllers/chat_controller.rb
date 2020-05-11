f609cf6b7dc2d8d2e7e57454a5de60beec186034
Michael Wayne <mw00885@surrey.ac.uk>
2020-05-11 20:56:09 +0100

    Chat fix

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -4,10 +4,13 @@ class ChatController < ApplicationController
     session[:conversations] ||= []
 
     if !Homeowner.find_by_user_id(current_user.id).nil?
-      @homeowner_conversations_with = Driver.all.where(:id => Booking.select(:driver_id).all.where(:homeowner_id => Homeowner.find_by_user_id(current_user.id).id, :complete => false))
+      @homeowner_conversations_with = User.all.where(:id => Driver.select(:user_id).all.where(:id => Booking.select(:driver_id).all.where(:homeowner_id => Homeowner.find_by_user_id(current_user.id).id, :complete => false)))
     end
-    @driver_conversations_with = Homeowner.all.where(:id => Booking.select(:homeowner_id).all.where(:driver_id => Driver.find_by_user_id(current_user.id).id, :complete => false))
 
+    if !Driver.find_by_user_id(current_user.id).nil?
+      @driver_conversations_with = User.all.where(:id => Homeowner.select(:user_id).all.where(:id => Booking.select(:homeowner_id).all.where(:driver_id => Driver.find_by_user_id(current_user.id).id, :complete => false)))
+    end
+    
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
 
