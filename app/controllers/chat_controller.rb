d4b905dc3ecf3851596f0d385b093f1b497da5f5
Michael Wayne <mw00885@surrey.ac.uk>
2020-05-13 21:41:15 +0100

    Added comments for the chat and conversations controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,17 +3,18 @@ class ChatController < ApplicationController
 
     session[:conversations] ||= []
 
+    # Get the drivers which have booked with the current user, provided that the current user is a homeowner
     if !Homeowner.find_by_user_id(current_user.id).nil?
       @homeowner_conversations_with = User.all.where(:id => Driver.select(:user_id).all.where(:id => Booking.select(:driver_id).all.where(:homeowner_id => Homeowner.find_by_user_id(current_user.id).id, :complete => false)))
     end
 
+    # Get the homeowners which the current user has booked with, provided that the current user is a driver
     if !Driver.find_by_user_id(current_user.id).nil?
       @driver_conversations_with = User.all.where(:id => Homeowner.select(:user_id).all.where(:id => Booking.select(:homeowner_id).all.where(:driver_id => Driver.find_by_user_id(current_user.id).id, :complete => false)))
     end
 
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
-
 
   end
 end
